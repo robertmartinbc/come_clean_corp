0e954b76d0c0640e551070533ed298e042b6f0d9
Robert Martin <robertmartinbc@shaw.ca>
2013-10-09 21:56:55 -0700

    First commit nad README update.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComeCleanCorp::Application.config.secret_token = 'e3e16f6ff9b6128f5bd4dc2af68093df64fbfe7a669ad2737226d22a240be5b24b351bb302c5942fad2784de19f70e0442b6a23b8af34cac236209dd4fdc353b'
